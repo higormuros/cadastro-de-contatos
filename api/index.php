2835b4346a9d37d2a3b1d6f2d63f607eaf492454
Higor Muros <96095355+higormuros@users.noreply.github.com>
2022-07-25 10:18:24 -0300

    Limpeza do código
    Testes antigos foram transformados em comentários. Estes comentários foram apagados porque não serão mais necessários

--- api/index.php
+++ api/index.php
@@ -1,19 +1,5 @@
 <?php
 
-//require "../../cadastro-contatos-privado/modelos/usuario.php";
-//$u=new usuario("Higor de souza","higorsouza@teste.com","(21) 99999-9999","1988-04-09","São Gonçalo-RJ");
-//$u->inserir();
-//var_dump($u);
-/*usuario::atualizar(2,array(
-	"nome"=>"Higor Teixeira",
-	"email"=>"higor.teste@gmail.com",
-	"telefone"=>"(21) 97913-7202",
-	"nascimento"=>"2000-04-09",
-	"cidade"=>"São Gonçalo"
-));
-echo "<pre>";
-var_dump(usuario::select("nome","Higor Teixeira"));
-echo "</pre>";*/
 $diretorio="../../../cadastro-contatos-privado/controladores/";
 if (isset($_REQUEST["acao"])){
 	switch ($_REQUEST["acao"]){
