adf029b8f2f8fb72f66f64ed6c5ad5011b74b146
Higor Muros <96095355+higormuros@users.noreply.github.com>
2022-07-25 21:00:36 -0300

    Impedir envio do formulário com ENTER
    Isso faz com que a resposta seja limpa, dando a impressão de que dados não foram encontrados.

--- cadastro-contatos-cliente/index.php
+++ cadastro-contatos-cliente/index.php
@@ -74,6 +74,14 @@
 			</div>
 		</div>
 		<script>
+		$(document).ready(function() {
+			$(window).keydown(function(event){
+					if(event.keyCode == 13) {
+						event.preventDefault();
+						return false;
+					}
+				});
+			});
 			function enviarForm(){
 				coluna=document.getElementById("coluna").value;
 				criterio=document.getElementById("criterio").value;
